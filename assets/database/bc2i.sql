e48d54805a3068fc6af426de27ad6b13a694fbfd
Sach <navodatasacchu26@gmail.com>
2019-12-12 19:28:11 +0530

    Update bc2i.sql

--- assets/database/bc2i.sql
+++ assets/database/bc2i.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2015 at 01:53 PM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Generation Time: Dec 12, 2019 at 02:55 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,8 +19,28 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ocm`
+-- Database: `bc2i`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `adminid` varchar(15) NOT NULL,
+  `password` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `adminid`, `password`) VALUES
+(1, 'Sachin', 'BC2ISACH2019', '8970081804Sach$');
 
 -- --------------------------------------------------------
 
@@ -26,30 +48,46 @@ SET time_zone = "+00:00";
 -- Table structure for table `complaint`
 --
 
-CREATE TABLE IF NOT EXISTS `complaint` (
-  `cid` int(10) NOT NULL,
-  `fname` varchar(55) NOT NULL,
-  `lname` varchar(55) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `device` varchar(52) NOT NULL,
-  `manufacturer` varchar(55) NOT NULL,
-  `model` varchar(55) NOT NULL,
-  `warranty` varchar(10) NOT NULL,
-  `complaint` varchar(555) NOT NULL,
-  `technician` varchar(55) DEFAULT NULL,
-  `status` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+CREATE TABLE `complaint` (
+  `id` int(11) NOT NULL,
+  `cid` varchar(12) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `doc` varchar(20) NOT NULL COMMENT 'Date of Complaint',
+  `toc` varchar(20) NOT NULL COMMENT 'Time of Complaint',
+  `subject` varchar(100) NOT NULL,
+  `doi` varchar(20) NOT NULL COMMENT 'Date of Incident',
+  `toi` varchar(20) NOT NULL COMMENT 'Time of Incident',
+  `type` varchar(20) NOT NULL COMMENT 'Type of Crime',
+  `complaint` text NOT NULL,
+  `status_num` int(11) NOT NULL DEFAULT 1 COMMENT 'Status number  1 - Pending  2-Case Filed ',
+  `status` varchar(20) NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `complaint`
 --
 
-INSERT INTO `complaint` (`cid`, `fname`, `lname`, `email`, `device`, `manufacturer`, `model`, `warranty`, `complaint`, `technician`, `status`) VALUES
-(1, 'sdvsdv', 'asdvs', 'gdsfgds@afbhsd.com', 'mobile', 'sdvDV', 'sdfsdgf', 'yes', 'sdgsgfdhgsdg', NULL, NULL),
-(2, 'esagdehg', 'as', 'afsdh', 'mobile', 'sgag', 'sgafd', 'no', 'asdhgdah', NULL, NULL),
-(3, 'ajinkya', 'gutti', 'ajinkya@mymail.com', 'tv', 'lg', 'lg lf540a', 'no', 'display flicker', 'C', NULL),
-(4, 'ajinkya', 'gutti', 'ajinkya@mymail.com', 'tv', 'lg', 'lg lf540a', 'no', 'display flicker', 'B', NULL),
-(8, 'harish', 'kandan', 'harish@mymail.com', 'home', 'whirpool', 'washing machine222', 'yes', 'not working', 'A', NULL);
+INSERT INTO `complaint` (`id`, `cid`, `uid`, `email`, `doc`, `toc`, `subject`, `doi`, `toi`, `type`, `complaint`, `status_num`, `status`) VALUES
+(27, '20191211W1PK', 22, 'satish@gmail.com', '11/12/2019', '10:11:08am', 'something happened at some place ', '2019-11-23', '08:56', 'Hacking', 'dsfgerytrsyr', 2, 'Case Filed');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `counter`
+--
+
+CREATE TABLE `counter` (
+  `id` int(11) NOT NULL DEFAULT 1,
+  `visits` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `counter`
+--
+
+INSERT INTO `counter` (`id`, `visits`) VALUES
+(1, 68);
 
 -- --------------------------------------------------------
 
@@ -57,55 +95,78 @@ INSERT INTO `complaint` (`cid`, `fname`, `lname`, `email`, `device`, `manufactur
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `uid` int(10) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(50) NOT NULL,
   `sex` varchar(10) NOT NULL,
-  `dob` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `dob` varchar(50) NOT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `num_comp` int(11) NOT NULL DEFAULT 0 COMMENT 'this is set after the complaint  has been registered by the user'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`uid`, `name`, `email`, `password`, `sex`, `dob`) VALUES
-(1, 'admin', 'admin@admin.com', 'admin', 'm', '01-01-01'),
-(2, 'AjinkyaGutti', 'ajinkya@mymail.com', '12345', 'Male', '2015-10-16'),
-(3, 'DevendraKamalapurkar', 'devendra@mymail.com', '12345', 'Male', '2015-10-16'),
-(4, 'Harishkandan', 'harish@mymail.com', '12345', 'Male', '2015-10-16');
+INSERT INTO `user` (`uid`, `name`, `email`, `password`, `sex`, `dob`, `phone`, `num_comp`) VALUES
+(1, 'admin', 'admin@admin.com', 'admin', 'm', '01-01-01', NULL, 0),
+(14, 'Sachin S Koparde', 'contactsachinkoparde@gmail.com', '1234', '', '', '8970081804', 0),
+(22, 'Satish', 'satish@gmail.com', '123456', '', '', '8974458945', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `complaint`
 --
 ALTER TABLE `complaint`
-  ADD PRIMARY KEY (`cid`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `counter`
+--
+ALTER TABLE `counter`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`uid`);
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `complaint`
 --
 ALTER TABLE `complaint`
-  MODIFY `cid` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `uid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
